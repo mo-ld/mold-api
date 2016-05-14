33400984f85767f1405e91b79832326e35184c7a
Maxime Déraspe <maximilien1er@gmail.com>
2016-05-13 17:20:13 -0700

    edit default with short notice
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- api/default.rb
+++ api/default.rb
@@ -1,8 +1,8 @@
 # default routes
 MyApp.add_route("GET","/") do
-  {}.to_json
+  "Need to provide the version (/v1/) and one of the following call : search, describe, inlinks, outlinks, sparql"
 end
 
 MyApp.add_route("GET","/v1/") do
-  {}.to_json
+  "Try one of the following call : search, describe, inlinks, outlinks, sparql"
 end
